47e9c5118da683b374079aee61c0e9804117d73d
Dmytro-Soroka <dimasoroka2808@gmail.com>
2020-11-24 13:31:24 +0200

    додав опис завдань

--- js/task-3.js
+++ js/task-3.js
@@ -9,3 +9,19 @@ function findLongestWord(string = "") {
   }
   return longestWord;
 }
+
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+// 'jumped'
+
+console.log(findLongestWord('Google do a roll'));
+// 'Google'
+
+console.log(findLongestWord('May the force be with you'));
+// 'force'
+
+
+/*
+Задача 2-3
+Поиск самого длинного слово в строке с пробелами
+Напиши функцию findLongestWord(string = ""), которая принимает параметром произвольную строку (в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.
+*/
